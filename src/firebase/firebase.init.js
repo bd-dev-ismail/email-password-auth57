964b650454db07194c19fa50e4fa4c4775f0d5ac
bd-dev-ismail <ismailratul7@gmail.com>
2022-10-13 23:23:52 +0600

    start project email & password sign In

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBR_t0tfRvKKmCpL3lB-xRpKIT-cbFo8l0",
+  authDomain: "email-password-auth-cd32f.firebaseapp.com",
+  projectId: "email-password-auth-cd32f",
+  storageBucket: "email-password-auth-cd32f.appspot.com",
+  messagingSenderId: "177800340791",
+  appId: "1:177800340791:web:caa756e33f818d450966b2",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
